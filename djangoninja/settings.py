fd3129f200b936f7e0175bcd9243bf161b2c340a
KooHyunJung <ghj3160@gmail.com>
2022-02-10 09:57:28 +0900

    Execution mypy

--- djangoninja/settings.py
+++ djangoninja/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from typing import List
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-r_petr_i@rjxx-3m^-rjmxjxr2oe9(-on+rwn_tpdl!fl$hwez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
