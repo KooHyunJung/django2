c702d7bcf70bee6794e85c0ea3c8591c7796474e
KooHyunJung <ghj3160@gmail.com>
2022-02-11 21:00:00 +0900

    run ./test.sh

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,12 +1,19 @@
-from django.db.models import QuerySet
+from django.db.models import Prefetch, QuerySet
 
-from tabom.models import Article
-
-
-def get_an_article(article_id: int) -> Article:
-    article = Article.objects.filter(id=article_id).get()
-    return article
+from tabom.models import Article, Like
 
 
-def get_article_list(offset: int, limit: int) -> QuerySet[Article]:
-    return Article.objects.order_by("-id").prefetch_related("like_set")[offset : offset + limit]
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
+
+
+def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
+    return (
+        Article.objects.order_by("-id")
+        .prefetch_related("like_set")
+        .prefetch_related(Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes"))[
+            offset : offset + limit
+        ]
+    )
